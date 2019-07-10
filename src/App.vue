385792fbf35faa5f4147be85b02d3ffe7c1259a9
Bald Mike <baldmike312@gmail.com>
2019-07-10 17:28:06 -0500

    csv parse to json

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,47 @@
 <template>
 
     <div id="app">
-        <b-container-fluid>
+        <div class="container-fluid">
             <b-row>
                 <div class="col-4">
                     <input-component/>
-                    <filter-component/>
+                    
                 </div>
                 
                 <div class="col-8">
                     <gmaps-component/>
                 </div>
             </b-row>
-        </b-container-fluid>
+        </div>
     </div>
 </template>
 
 <script>
 
-import GmapsComponent from './components/GmapsComponent.vue'
-import InputComponent from './components/InputComponent.vue'
-import FilterComponent from './components/FilterComponent.vue'
+    import GmapsComponent from './components/GmapsComponent.vue'
+    import InputComponent from './components/InputComponent.vue'
+    
 
-export default {
-name: 'app',
-components: {
-GmapsComponent,
-InputComponent,
-FilterComponent
+    export default {
+        name: 'app',
+        components: {
+            GmapsComponent,
+            InputComponent,
+            
+        }
+    }
 
-}
-}
 </script>
 
 <style>
-#app {
-font-family: 'Avenir', Helvetica, Arial, sans-serif;
--webkit-font-smoothing: antialiased;
--moz-osx-font-smoothing: grayscale;
-text-align: center;
-color: #2c3e50;
-margin-top: 60px;
-}
+
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
+
 </style>
--- src/components/InputComponent.vue
+++ src/components/InputComponent.vue
@@ -1,11 +1,86 @@
 <template>
-    <h2>Input Component</h2>
+    <div class="container">
+        <b-form-file
+        v-model="csvFile"
+        :state="Boolean(csvFile)"
+        placeholder="Select CSV file"
+        drop-placeholder="Drop file here..."
+        @change="onFileChange"
+        ></b-form-file>
+
+        <div class="mt-3">Selected file: {{ csvFile ? csvFile.name : '' }}</div>
+
+        
+    </div>
 </template>
 
 <script>
-export default {
-    
-}
+
+    import Papa from 'papaparse'
+
+    export default {
+        name: "input-component",
+        
+        data() {
+            return {
+                csvFile: null
+            }
+        },
+        
+        methods: {
+            onFileChange(e) {
+
+                const file = e.target.files[0];
+                
+                console.log(file);
+
+                const that = this
+                const fileToLoad = event.target.files[0]
+                const reader = new FileReader()
+                reader.onload = fileLoadedEvent => {
+                Papa.parse(fileLoadedEvent.target.result, {
+                    header: true,
+                    complete (results) {
+                    console.log('Parse Complete', results)
+                    that.doc = JSON.stringify(results.data, null, 2)
+                    },
+                    error (errors) {
+                    console.log('error', errors)
+                    }
+                })
+                }
+                reader.readAsText(fileToLoad)
+
+
+                // NO NOT REMOVE -- THIS SHOWS ALL OF PAPA PARSE'S DEFAULTS
+                // Papa.parse(file, {
+                //     delimiter: "",	// auto-detect
+                //     newline: "",	// auto-detect
+                //     quoteChar: '"',
+                //     escapeChar: '"',
+                //     header: false,
+                //     transformHeader: undefined,
+                //     dynamicTyping: false,
+                //     preview: 0,
+                //     encoding: "",
+                //     worker: false,
+                //     comments: false,
+                //     step: undefined,
+                //     complete: undefined,
+                //     error: undefined,
+                //     download: false,
+                //     downloadRequestHeaders: undefined,
+                //     skipEmptyLines: false,
+                //     chunk: undefined,
+                //     fastMode: undefined,
+                //     beforeFirstChunk: undefined,
+                //     withCredentials: undefined,
+                //     transform: undefined,
+                //     delimitersToGuess: [',', '\t', '|', ';', Papa.RECORD_SEP, Papa.UNIT_SEP]
+                // })
+            },
+        }
+    }
 </script>
 
 <style scoped>
